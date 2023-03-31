d87b0430ba3d7098819499b1e8eb6be644728d55
getCrowded <impoution@gmail.com>
2023-04-01 02:31:55 +0500

    🎉

--- crowded/urls.py
+++ crowded/urls.py
@@ -26,7 +26,7 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib.sitemaps.views import sitemap
-from ground.sitemaps import StaticViewSitemap, GroundSitemap , Sitemap
+from ground.sitemaps import StaticViewSitemap, GroundSitemap
 
 sitemaps = {
     'static': StaticViewSitemap,
